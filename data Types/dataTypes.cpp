0f2d970a1bb35da5140944c37d13193386db03b1
Rishabh Singh <rishabhsingh50689@gmail.com>
2022-12-11 18:26:46 +0530

    initial push

---
+++ data Types/dataTypes.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main() {
+    int a;                  // 4 bytes  ---- negative 2 pow 31 to positive 2 pow 31 
+    unsigned int b;         // 4 bytes  ---- positive 2 pow 32 
+    char c;                 // 1 byte   ---- used to store a single character
+    short d;                // 2 bytes integer
+    long e;                 // 8 bytes integer
+    float f;                // 4 bytes floating point 
+    double g;               // 8 bytes floating point 
+    bool h;                 // 1 bit , values: 0 , 1 , true, false
+    string s = "hello world";
+    char hello[5] = {'H','e','e','l','o'}; // character array
+    char ans[] = "hello";
+
+
+    return 0;
+}
---
+++ switch/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int x = 2 ;
+
+    switch (x)              // based on the values of x 
+    {
+    case 2:                 // if x is 2 then execute this
+        cout << "Two";
+        break;
+    
+    default:
+        break;
+    }
+}
